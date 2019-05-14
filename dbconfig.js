844a2b5a93e9d13c5f02ef6c1762d3243dd345de
unknown <jadismail7@gmail.com>
2019-05-14 17:09:49 +0300

    maba3ref

--- dbconfig.js
+++ dbconfig.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql');
 
 var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'research_hub'
+    host: '51.77.192.7',
+    user: 'commandeers',
+    password: 'commandeers1234',
+    database: 'SCIENTIFIC_HUB'
 });
 
+setInterval(()=>{
+    //Because the services shut down if it is idle.
+    con.query("Select 1");
+},10000)
 
-module.exports = con ;
+module.exports = con;
--- index.js
+++ index.js
@@ -5,8 +5,12 @@ var app = express();
 app.use(express.json())
 // app.use(bodyParser);
 
-app.post('/api/add/scientist',function(req,res){
-    con.query(`INSERT INTO scientist VALUES(${req.body.id},'${req.body.name}','${req.body.fName}',${req.body.salary},${req.body.managerID},${req.body.depID})`,(err,result)=>{
+app.post('/api/add/scientist',(req,res)=>{
+    var manager = 'NULL';
+    if (req.body.managerID != null) {
+        manager = req.body.managerID;
+    }
+    con.query(`INSERT INTO scientist VALUES('${req.body.name}','${req.body.fName}',${req.body.salary},${req.body.managerID},${req.body.depID})`,(err,result)=>{
         if (err) throw err;
         console.log("successful");
         res.end("successful");
@@ -14,24 +18,27 @@ app.post('/api/add/scientist',function(req,res){
 });
 
 
-app.post('/api/add/project',function(req,res){
-    con.query(`INSERT INTO project VALUES(${req.body.id},'${req.body.name}','${req.body.fName}',${req.body.salary},${req.body.managerID},${req.body.depID})`,(err,result)=>{
+app.post('/api/add/project',(req,res)=>{
+    con.query(`INSERT INTO project VALUES('${req.body.name}',${req.body.managerID},${req.body.depID})`,(err,result)=>{
         if (err) throw err;
         console.log("successful");
         res.end("successful");
     });
 });
 
-app.get('/api/getall/scientists',function(req,res){
+app.post("/api/add/resource",(req,res)=>{
+    
+})
+
+app.get('/api/getall/scientists',(req,res)=>{
         con.query('SELECT * FROM `scientist` ', (err,result) => {
         if(err) throw err;
         console.log(JSON.stringify(result));
         res.end(JSON.stringify(result));
     });
-    
 });
 
-app.get('/api/get/scientists/:project',function(req,res){
+app.get('/api/get/scientists/:project',(req,res)=>{
     con.query(`SELECT * FROM scientist s INNER JOIN scientist_project_participation sp ON s.ID = sp.ScientistID INNER JOIN project p on p.ProjID = sp.ProjectID WHERE p.Name = '${req.params.project}' `,(err,result)=>{
         if (err) throw err;
         console.log(JSON.stringify(result));
@@ -43,26 +50,3 @@ app.get('/api/get/scientists/:project',function(req,res){
 
 
 app.listen(8080,()=>{console.log('listening on port 8080')});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- // Object.keys(result).forEach(function(key) {
-        //     var row = result[key];
-        //     console.log(JSON.stringify(row)); 
-        // });
-        // console.log(result);
-        // res.end(result);
