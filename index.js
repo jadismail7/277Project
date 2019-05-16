a42ea9ab0046936686922b936e1c125cd8e43700
unknown <jadismail7@gmail.com>
2019-05-16 14:57:47 +0300

    removed cors

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ app.use(express.json())
 app.use((req, res, next)=>{
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials","true");
   next();
 });
 
-app.use(cors({
-    origin:['*'],
-    methods:['GET','POST','PUT','DELETE'],
-    credentials: true // enable set cookie
-}));
+// app.use(cors({
+//     origin:['*'],
+//     methods:['GET','POST','PUT','DELETE'],
+//     credentials: true // enable set cookie
+// }));
 
 app.use(session({
     secret: "keyboardcat",
