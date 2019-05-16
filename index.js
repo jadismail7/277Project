eb5218c546a95f672ec633c3f60461f8f8b00cf8
unknown <jadismail7@gmail.com>
2019-05-16 14:50:33 +0300

    added cors

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.use((req, res, next)=>{
   next();
 });
 
+app.use(cors({
+    origin:['*'],
+    methods:['GET','POST','PUT','DELETE'],
+    credentials: true // enable set cookie
+}));
+
 app.use(session({
     secret: "keyboardcat",
     name: "mycookie",
