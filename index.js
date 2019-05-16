2d6931e351776e3039d487158dd5be35ed0103ae
unknown <jadismail7@gmail.com>
2019-05-16 14:32:52 +0300

    resave: false

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use((req, res, next)=>{
 });
 
 app.use(session({secret: 'alildeektimes',
-                resave: true,
+                resave: false,
                 saveUninitialized: true,
                 cookie: {secure: true}
             })
